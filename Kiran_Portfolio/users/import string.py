ceacf40d12a710b7b95589d2cec3f5a4dacf9413
bvkiran8058 <bvkirangoud1234@gmail.com>
2025-10-28 19:43:53 +0530

    modified users models

---
+++ Kiran_Portfolio/users/import string.py
@@ -0,0 +1,6 @@
+import string
+
+special_characters = string.punctuation + string.ascii_letters + string.
+print(list(special_characters))
+print(list(string.ascii_letters))
+
--- Kiran_Portfolio/users/models.py
+++ Kiran_Portfolio/users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import string
 
+special_characters = list(string.punctuation)
+string.
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email',
+        unique=True,
+        blank=False,
+        help_text= 'enter your valid email',
+        error_messages={
+            "unique": ("A user with that email already exists."),
+        },
+    )
+    password = models.CharField(
+        'password',
+        help_text='Enter your password which has min length of 8 and special char and number and caps and small letter'
+    )
+    role = models.CharField(
+        max_length=15,
+        help_text= 'select anyone of these (`Admin`, `Visitor`, `Recruiter`, `Contributor`)',
+        choices=[
+            ('admin', 'admin'),
+            ('visitor', 'visitor'),
+            ('recruiter', 'recruiter'),
+            ('contributor', 'contributor')
+        ]
+    )
+    email_verified = models.BooleanField(default=False)
+    profile_completed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.password is not None:
+            if all()
+        super().save(*args, **kwargs)
