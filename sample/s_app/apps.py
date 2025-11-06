2a689cb0383ed21c46cb93b934438f10b3c4297c
bvkiran8058 <bvkirangoud1234@gmail.com>
2025-11-06 23:09:01 +0530

    dummy project deleted

--- sample/s_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 's_app'
--- sample/s_app1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SApp1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 's_app1'
